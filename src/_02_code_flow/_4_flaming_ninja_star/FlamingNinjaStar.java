151cd30bcac086f0d702383c40b5583f3a845648
root <root@905a6c83cceb>
2022-10-02 21:24:28 +0000

    greeter

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,16 +10,30 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+		Robot Pallav = new Robot();
+		Pallav.penDown();
 		// Set the robot speed to 100
-		
+		Pallav.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+		Pallav.setWindowSize(800, 800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int Javoo = 0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+		while (Javoo < 1) {
+			Pallav.turn(45);
+			Pallav.move(64);
+			Pallav.turn(-40);
+			Pallav.setPenColor(Color.YELLOW);
+			Pallav.move(flameSize);
+			Pallav.turn(170);
+			Pallav.move(flameSize);
+			Pallav.turn(64);
+			Pallav.setPenColor(Color.BLACK);
+			Pallav.move(baseSize);
+			Javoo += 1;
+		}
+	
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
 		
 			   // MOVE           Move the robot 64 pixels
@@ -42,7 +56,7 @@ public class FlamingNinjaStar {
 		
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
-		
+	
 		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
 		//        Run the program again. Check the second picture in the recipe.
 
